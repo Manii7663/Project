5d2f315b44c64519495c164b804f3245227827fe
manii7663 <manishabhandari119@gmail.com>
2024-03-28 18:37:56 +0530

    deleted

--- server/Routes/routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { logIn } = require('../Controller/Auth');
-
-// POST request to handle user login
-router.post('/logIn', logIn);
-
-module.exports = router;
