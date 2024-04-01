97ee2dd8fa88fc93abc34f9d60559ea30da303d4
Manisha bhandari <manishabhandari@jmangroup.com>
2024-04-01 06:01:58 -0700

    Sign in page

--- client/src/Pages/Login/Login.js
+++ client/src/Pages/Login/Login.js
@@ -1,61 +1,107 @@
-import React from "react";
-import './Login.css';
-import  axios  from "axios";
-import { useNavigate } from 'react-router-dom';
-import { useState } from "react";
-
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import "./Login.css";
 
 function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-    const [email, setEmail] = useState("");
-	const [password, setPassword] = useState("");
+  const navigate = useNavigate();
 
-	const navigate = useNavigate();
+  const loginValidate = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await axios.post("http://localhost:3001/logIn", {
+        email,
+        password,
+      });
+      if (res.status === 201) {
+        console.log("validated successfully");
+        navigate("/");
+      }
+    } catch (err) {
+      console.log("error encountered");
+      console.log(err);
+      if (err.response.status === 401 || err.response.status === 402) {
+        alert(err.response.data.error);
+      } else {
+        alert("Internal Server Error");
+      }
+    }
+  };
 
-    const loginValidate = async (e) => {
-		e.preventDefault()
-		try {
-            
-			const res = await axios.post("http://localhost:3001/logIn", { email, password })
-			if (res.status === 201) {
-				// localStorage.setItem('id', res.data.user._id)
-				// localStorage.setItem('name', res.data.user.name)
-                console.log("validated succesfully")
-				navigate('/')
-			}
-		} catch (err) {
-            console.log("error encounterd")
-			console.log(err)
-			if (err.response.status === 401 || err.response.status === 402) {
-				alert(err.response.data.error)
-			}
-			else {
-				alert("Internal Server Error")
-			}
-		}
-	}
   return (
-  <div>
-    <div class="wrapper">
-        <div class="text-center mt-4 name">
-            Log In
-        </div>
-        <form class="p-3 mt-3" onSubmit={loginValidate}>
-            <div class="form-field d-flex align-items-center">
-                <span class="far fa-user"></span>
-                <input type="email" name="userName" id="userName" placeholder="Username" onChange={(e) => setEmail(e.target.value)}/>
+    <section className="background-radial-gradient overflow-hidden">
+      <div className="container px-4 py-5 px-md-5 text-center text-lg-start my-5">
+        <div className="row gx-lg-5 align-items-center mb-5">
+          <div className="col-lg-6 mb-5 mb-lg-0" style={{ zIndex: 10 }}>
+            <h1
+              className="my-5 display-5 fw-bold ls-tight"
+              style={{ color: "hsl(218, 81%, 95%)" }}
+            >
+              The best offer <br />
+              <span style={{ color: "hsl(218, 81%, 75%)" }}>
+                for your business
+              </span>
+            </h1>
+            <p
+              className="mb-4 opacity-70"
+              style={{ color: "hsl(218, 81%, 85%)" }}
+            >
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+              Temporibus, expedita iusto veniam atque, magni tempora mollitia
+              dolorum consequatur nulla, neque debitis eos reprehenderit quasi
+              ab ipsum nisi dolorem modi. Quos?
+            </p>
+          </div>
+
+          <div className="col-lg-6 mb-5 mb-lg-0 position-relative">
+            <div
+              id="radius-shape-1"
+              className="position-absolute rounded-circle shadow-5-strong"
+            ></div>
+            <div
+              id="radius-shape-2"
+              className="position-absolute shadow-5-strong"
+            ></div>
+
+            <div className="card bg-glass ">
+              <div className="card-body px-4 py-0 px-md-2 "></div>
+              
+              <form class="p-5 mt-0" onSubmit={loginValidate}>
+              <div class="text-center name">Log In</div>
+                <div class="form-field d-flex align-items-center">
+                  <span class="far fa-user"></span>
+                  <input
+                    type="email"
+                    name="userName"
+                    id="userName"
+                    placeholder="Username"
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
+                </div>
+                <div class="form-field d-flex align-items-center">
+                  <span class="fas fa-key"></span>
+                  <input
+                    type="password"
+                    name="password"
+                    id="pwd"
+                    placeholder="Password"
+                    onChange={(e) => setPassword(e.target.value)}
+                  />
+                </div>
+                <button type="submit" className="btn btn-primary mb-4">
+                Sign in
+              </button>
+              </form>
+
+              
             </div>
-            <div class="form-field d-flex align-items-center">
-                <span class="fas fa-key"></span>
-                <input type="password" name="password" id="pwd" placeholder="Password" onChange={(e) => setPassword(e.target.value)}/>
-            </div>
-            <button class="btn mt-3" type="submit">Login</button>
-        </form>
-        <div class="text-center fs-6">
-            <a href="#">Forget password?</a> or <a href="#">Sign up</a>
+          </div>
         </div>
-    </div>
-  </div>
+      </div>
+    </section>
   );
 }
 
